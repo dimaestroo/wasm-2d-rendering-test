107b3fd9eba59a00fbd7c7e8fa27eaf6b607b1a2
Agam Syahputra <agambaradja68@gmail.com>
2026-06-04 16:24:53 +0700

    fix webpack

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,14 +1,41 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
+  mode: "development",
+
   entry: "./bootstrap.js",
+
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
+    clean: true,
   },
-  mode: "development",
+
+  experiments: {
+    asyncWebAssembly: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/,
+        type: "webassembly/async",
+      },
+    ],
+  },
+
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "index.html", to: "index.html" },
+      ],
+    }),
   ],
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+  },
 };
